77e7006c72e7d2c16c35a70b4bc64ef4029d5b54
prijacash <prijacash@gmail.com>
2022-09-23 09:46:59 -0700

    finished deliverable pt 1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyList from './MyList';
+import './index.css';
+
+const toDos = ["Listen to Hot Mulligan", "Drink hot cocoa", "Eat hot cheetos"]
+
+ReactDOM.render(
+  <MyList 
+    theList={toDos}
+  />,
+  document.getElementById('root')
+)
+
